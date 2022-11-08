0bbd69431c3dd005a0ff48f7f589aad30ea2c9a6
Will Harvey <william.harvey@glasgow.ac.uk>
2022-11-08 14:38:00 +0000

    Function - locate the positions of N-linked glycosylation motifs

---
+++ toolkitSeqTree/R/Nglyc_locate.R
@@ -0,0 +1,33 @@
+#' N-linked glycosylation site locations
+#'
+#' Given an amino acid sequence, identify the positions of potential N-linked glycosylation sites.
+#' The N-linked glycosylation motif is N-X-S/T or Asn-X-Ser/T where X is any amino acid
+#' except Proline (Pro/P).
+#'
+#' @param sequence string representing amino acid sequence
+#' @param allow_dash logical, should dash be allowed in the middle position of the motif
+#' @param allow_X logical, should X be allowed in the middle position of the motif
+#'
+#' @return the count of N-linked glycosylation sites identified
+#' @export
+#'
+#' @examples
+#' sequence <- 'MDTICIGHAYNNPALEKNKSSTDATNLTYYG'
+#' Nglyc_locate(sequence)
+Nglyc_locate <- function(sequence, allow_dash = F, allow_X = F) {
+
+  sequence <- toupper(sequence)
+  # aa single letter code does not include BJOUXZ
+  # (U is selenocysteine)
+  if (allow_dash == T & allow_X == T) {
+    x <- stringr::str_locate_all(sequence, pattern = 'N[-ACDEFGHIKLMNQRSTVWXY][ST]')
+  } else if ((allow_dash == T & allow_X == F)) {
+    x <- stringr::str_locate_all(sequence, pattern = 'N[-ACDEFGHIKLMNQRSTVWY][ST]')
+  } else if ((allow_dash == F & allow_X == T)) {
+    x <- stringr::str_locate_all(sequence, pattern = 'N[ACDEFGHIKLMNQRSTVWXY][ST]')
+  } else if ((allow_dash == F & allow_X == F)) {
+    x <- stringr::str_locate_all(sequence, pattern = 'N[ACDEFGHIKLMNQRSTVWY][ST]')
+  }
+  return(x)
+}
+
