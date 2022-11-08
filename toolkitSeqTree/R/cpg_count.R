72875aa2ff14f2adbe53729d049560216bd35dfe
Will Harvey <william.harvey@glasgow.ac.uk>
2022-11-08 14:39:23 +0000

    Function - count CpGs in nucleotide sequence

---
+++ toolkitSeqTree/R/cpg_count.R
@@ -0,0 +1,17 @@
+#' CpG count
+#'
+#' Count CpG in a nucleotide sequence.
+#'
+#' @param sequence String representation of nucleotide sequence
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' sequence <- 'CATGCTGTGGTCGCGTAGGGTTATTCGTCGTAATTGCAGCT'
+#' cpg_count(sequence, method = 2, res_expand = T)
+cpg_count <- function(sequence = NA) {
+  sequence <- toupper(sequence)
+  n_CpG <- stringr::str_count(sequence, pattern = 'CG')
+  n_CpG
+}
