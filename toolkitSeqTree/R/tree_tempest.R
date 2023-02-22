f336c3bbd7d106a68d2b6851d048137363c00331
Will Harvey <wharvey.evo@gmail.com>
2023-02-22 10:59:39 +0000

    Function for examining temporal signal updated

--- toolkitSeqTree/R/tree_tempest.R
+++ toolkitSeqTree/R/tree_tempest.R
@@ -14,7 +14,7 @@ tree_tempest <- function(tree_dat = NA, dates = NA) {
 
   if (is.na(dates)) {
     res <- tree_dat[tree_dat$isTip == T,
-                    c('label', 'date')]
+                    c('label','x',  'date')]
     res$root_dist <- NA
   }
 
@@ -33,16 +33,34 @@ tree_tempest <- function(tree_dat = NA, dates = NA) {
     res$root_dist[i] <- sum(ancestor_lengths)
   }
 
-  ###
+  ### Scatter plot showing correlation between date distance and
   correlation <- round(cor(res$date, res$root_dist, method = 'pearson'), 3)
+  summary(lm(res$root_dist ~ res$date))
   plot <- ggplot2::ggplot(res, ggplot2::aes(x = date, y = root_dist)) +
-    ggplot2::geom_point() +
+    ggplot2::geom_point(pch = 21, colour = 'grey20', fill = 'slategrey') +
+    geom_smooth(formula = 'y ~ x', method = 'lm', se = F, col = 'grey20') +
     ggplot2::xlab('Date') +
-    ggplot2::ylab('Distance from root') +
+    ggplot2::ylab('Root-to-tip divergence') +
     ggplot2::labs(title = paste("Pearson's correlation coefficient:", correlation)) +
-    ggplot2::theme_minimal()
+    ggplot2::theme_minimal() +
+    ggplot2::theme(text = element_text(size = 13))
 
   print(plot)
-  cat("Pearson's correlation coefficient:\n", correlation)
+
+  res <- summary(lm('x ~ date', tree_dat))
+  res_yintercept <- res$coefficients[1]
+  res_slope <- res$coefficients[2]
+  res_xintercept <- (-res_yintercept) / res_slope
+  date_range <- max(tree_dat$date, na.rm = T) - min(tree_dat$date, na.rm = T)
+
+  ######## Print range of info on
+  cat('Date range:', round(date_range, 3), '\n')
+  cat('Slope (rate):', round(res_slope, 3), '\n')
+  cat('x-intercept (tMRCA):', round(res_xintercept, 2), '\n')
+  cat('Correlation coefficient:', round(sqrt(res$r.squared), 3), '\n')
+  cat('R squared:', round(res$r.squared, 3), '\n')
 
 }
+
+
+tree_tempest(tree_dat)
