6ded7dfa779bd72976fb8621c60d572dbdf9953f
Will Harvey <wharvey.evo@gmail.com>
2025-03-06 10:00:11 +0000

    Function to calculate internal node dates for beast trees or posterior sample of trees

---
+++ toolkitSeqTree/R/add_node_dates.R
@@ -0,0 +1,45 @@
+#' Add node dates
+#'
+#' Calculate dates of internal nodes of tree given tree structure and the
+#' most recent sampling date (mrsd). Can be done for single tree or for a
+#' posterior sample of trees (pst).
+#'
+#' `tree_dat` should have column `x` and in the case of PST, `.id` too.
+#'
+#' @param tree_dat Data frame describing tree structure
+#' @param mrsd date in format "YYYY-MM-DD"
+#' @param pst logical, does dataframe represent posterior sample of trees
+#'
+#' @returns version of tree_dat with columns `date` and `date_format`
+#' @export
+#'
+add_node_dates <- function(tree_dat = NA, mrsd = NA, pst = FALSE) {
+
+  MRSD_decimal <- ggtree::Date2decimal(MRSD)
+  tree_dat$date <- NA
+
+  ## if working with PST, need to account for different max x per tree
+  if (pst == TRUE) {
+    tree_ids <- unique(tree_dat$.id)
+    n_trees <- length(tree_ids)
+
+    # loop through trees in PTS
+    for (i in 1:n_trees) {
+      # id max x for current tree
+      curr_tree_id <- tree_ids[i]
+      curr_max_x <- max(tree_dat$x[which(tree_dat$.id == curr_tree_id)])
+      # if tree id matches curr_tree_id, calc date
+      tree_dat$date <- ifelse(tree_dat$.id == curr_tree_id,
+                              MRSD_decimal - (curr_max_x - tree_dat$x),
+                              tree_dat$date)
+    }
+  } else if (pst == FALSE) {
+    max_x <- max(tree_dat$x)
+    tree_dat$date <- MRSD_decimal - (max_x - tree_dat$x)
+  }
+
+  # also create column with formatted version of date
+  tree_dat$date_format <- ggtree::decimal2Date(tree_dat$date)
+
+  tree_dat
+}
